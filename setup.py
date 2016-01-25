de5564af1d5d16c61a036d43dc93be2a2f347627
Derek Ludwig <dludwig@continuum.io>
2016-01-25 10:27:31 -0800

    Add package setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='flask-kerberos-login',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Continuum Analytics',
+    author_email='dludwig@continuum.io',
+    url='http://github.com/ContinuumIO/flask-kerberos-login',
+    description='Kerberos authentication compatible with flask-login',
+    packages=find_packages(),
+    install_requires=[],
+)
+
