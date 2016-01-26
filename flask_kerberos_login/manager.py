99578cb10a0673e4e92391ccad53e90689d3bc9e
Derek Ludwig <dludwig@continuum.io>
2016-01-25 17:45:35 -0800

    Remove python-gssapi

--- flask_kerberos_login/manager.py
+++ flask_kerberos_login/manager.py
@@ -4,24 +4,22 @@ Provides a pluggable login manager that uses Kerberos for authentication
 from __future__ import absolute_import, print_function, unicode_literals
 
 import base64
-import socket
 import logging
+import socket
 
+from flask import _request_ctx_stack as stack
 from flask import abort
 from flask import request
 from flask import Response
-from flask import _request_ctx_stack as stack
 from werkzeug.exceptions import HTTPException
-
-# import gssapi
-
 import kerberos
 
-# log = logging.getLogger(__name__)
 
-log = None
+log = logging.getLogger(__name__)
 
-def negotiate(token):
+
+def negotiate(token=None):
+    '''Generate 'WWW-Authenticate' header value'''
     header = 'Negotiate'
     if token:
         header += ' ' + token
@@ -90,8 +88,9 @@ class KerberosLoginManager(object):
 
 
     def init_app(self, app):
-        global log
-        log = app.logger
+        '''
+        Initializes the extension with the application object
+        '''
         self.app = app
         app.kerberos_manager = self
         app.after_request(self.append_header)
@@ -112,43 +111,31 @@ class KerberosLoginManager(object):
 
 
     def append_header(self, response):
+        '''
+        Adds WWW-Authenticate header with SPNEGO challenge or Kerberos token
+        '''
         kerberos_token = getattr(stack.top, 'kerberos_token', None)
         if response.status_code == 401 or kerberos_token:
-            response.headers[b'WWW-Authenticate'] = negotiate(kerberos_token)
+            response.headers['WWW-Authenticate'] = negotiate(kerberos_token)
 
         return response
 
 
     def load_user(self, request):
-        header = request.headers.get(b'Authorization')
-        if header and header.startswith(b'Negotiate '):
-            in_token = base64.b64decode(header[10:])
-            in_token = header[10:]
+        '''
+        Extract a user from the current request
 
+        Raises:
+
+            HTTPException: 401 status if the authentication is incomplete
+        '''
+        header = request.headers.get(b'authorization')
+        if header and header.startswith(b'Negotiate '):
+
+            in_token = header[10:]
             rc = _gssapi_authenticate(in_token, self._service_name)
             if rc == kerberos.AUTH_GSS_COMPLETE:
                 return self._save_user(stack.top.kerberos_user)
             else:
                 abort(401)
-
-            # service_name = gssapi.Name(
-            #     b'{}@{}'.format(self.config['KRB_SERVICE_NAME'], self.config['KRB_REALM']),
-            #     gssapi.C_NT_HOSTBASED_SERVICE,
-            # )
-            # credential = gssapi.Credential(service_name, usage=gssapi.C_ACCEPT)
-            # ctx = gssapi.AcceptContext(credential)
-
-            # out_token = ctx.step(in_token)
-            # self.kerberos_token = base64.b64encode(out_token)
-
-            # if ctx.established:
-            #     # if self._save_user:
-            #     #     self._save_user()
-            #     return self._save_user(ctx.peer_name)
-            # else:
-            #     abort(401)
-        else:
-            return None
-
-
 
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'flask',
-        'python-gssapi',
+        'kerberos',
     ],
 )
 
