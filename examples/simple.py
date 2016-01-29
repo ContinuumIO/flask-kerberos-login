feffa87bd1ce675f4b0e2c90c15668988c1ee7d9
Derek Ludwig <dludwig@continuum.io>
2016-01-28 18:16:12 -0800

    Port and host for example should be configurable

--- examples/simple.py
+++ examples/simple.py
@@ -54,7 +54,8 @@ def home():
     return 'hello %s!' % current_user
 
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    host = os.environ.get('HOST', '127.0.0.1')
+    app.run(debug=True, port=port, host=host)
